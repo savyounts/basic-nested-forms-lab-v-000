6276020903a95251158322b59ec81fd11a6320e5
Savannah Younts <savyounts@gmail.com>
2018-09-24 19:21:59 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,4 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
   accepts_nested_attributes_for :ingredients
 
-  def ingredients=(ingredients)
-    ingredients.each do |i|
-      ingredient = Ingredient.find_or_create_by(name: name)
-      self.ingredients << ingredient
-    end
-  end
 end
