65317af0654cec6e4440a392b876b99e7802479b
Savannah Younts <savyounts@gmail.com>
2018-09-24 18:57:18 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "ingredient 1")
+    @recipe.ingredients.build(name: "ingredient 2")
   end
 
   def create
