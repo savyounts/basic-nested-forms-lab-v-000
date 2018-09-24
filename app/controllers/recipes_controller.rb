5a663651906dd2cc378d8b3f0b45bfbdef653dc1
Savannah Younts <savyounts@gmail.com>
2018-09-24 19:00:19 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,4 +17,9 @@ class RecipesController < ApplicationController
     @recipe = Recipe.create(recipe_params)
     redirect_to recipe_path(@recipe)
   end
+
+
+  private 
+   def recipe_params
+     params.require(:recipe).permit(:name, ingredients: [:name, :quantity])
 end
